4e1d0734e034f337dcf583c7e36f98b1777b8cb4
XimeM271 <l202407020@vbravo.tecnm.mx>
2026-04-25 20:11:43 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,217 +1,138 @@
 use anchor_lang::prelude::*;
-// ID del Solana Program, este espacio se llena automaticamente al haver el "build"
-declare_id!("");
 
-#[program] // Macro que convierte codigo de Rust a Solana. Apartir de aqui empieza tu codigo!
-pub mod biblioteca {
-    use super::*; // Importa todas los structs y enums definidos fuera del modulo
+// Puedes reemplazar este ID con el que te genere Solana Playground
+declare_id!("HYVAtyayLC2hPw96gCuHu1DZA4krAHmvmZwqbN4xFkqz");
 
-    //////////////////////////// Instruccion: Crear Biblioteca /////////////////////////////////////
-    /*
-    Permite la creacion de una PDA (Program Derived Adress), un tipo especial de cuenta en solana que permite prescindir 
-    del uso de llaves privadas para la firma de transacciones. 
+#[program]
+pub mod plant_care_solana {
+    use super::*;
 
-    Esta cuenta contendra el objeto (struct) de tipo Biblioteca donde podremos almacenar los Libros. 
-    La creacion de la PDA depende de 3 cosas:
-        * Wallet address 
-        * Program ID 
-        * string representativo, regularmente relacionado con el nombre del proyecto
-    
-    La explicacion de esto continua en el struct NuevaBiblioteca
-
-    Parametros de entrada:
-        * nombre -> nombre de la biblioteca -> tipo string
-     */
-    pub fn crear_biblioteca(context: Context<NuevaBiblioteca>, nombre: String) -> Result<()> {
-        // "Context" siempre suele ir como primer parametro, ya que permite acceder al objeto o cuenta con el que queremos interactuar
-        // Dentro del context va al tipo de objeto o cuenta con el que deseamos interactuar. 
-        let owner_id = context.accounts.owner.key(); // Accedemos al wallet address del caller 
-        msg!("Owner id: {}", owner_id); // Print de verificacion
-
-        let libros: Vec<Libro> = Vec::new(); // Crea un vector vacio 
-
-        // Creamos un Struct de tipo biblioteca y lo guardamos directamente 
-        context.accounts.biblioteca.set_inner(Biblioteca { 
-            owner: owner_id,
-            nombre,
-            libros,
-        });
-        Ok(()) // Representa una transaccion exitosa 
+    pub fn inicializar_invernadero(ctx: Context<CrearInvernadero>, nombre: String) -> Result<()> {
+        let invernadero = &mut ctx.accounts.invernadero;
+        invernadero.owner = ctx.accounts.owner.key();
+        invernadero.nombre_invernadero = nombre;
+        invernadero.plantas = Vec::new(); 
+        
+        msg!("Invernadero '{}' creado", invernadero.nombre_invernadero);
+        Ok(())
     }
 
-    //////////////////////////// Instruccion: Agregar Libro /////////////////////////////////////
-    /*
-    Agrega un libro al vector de libros ontenido en el struct Biblioteca. 
-    En este caso el contexto empleado es el struct NuevoLibro. Mientras que NuevaBiblioteca permite crear 
-    Instancias de una Biblioteca. NuevoLibro permite crear y modificar los valores relacionados a cualquier
-    struct de tipo Libro.
+    pub fn registrar_planta(
+        ctx: Context<GestionarInvernadero>, 
+        especie: String, 
+        volumen_ml: u16, 
+        frecuencia_dias: u8,
+        luz_directa: bool
+    ) -> Result<()> {
+        let invernadero = &mut ctx.accounts.invernadero;
+        
+        require!(invernadero.owner == ctx.accounts.owner.key(), Errores::NoEresElOwner);
 
-    Parametros de entrada:
-        * nombre -> nombre del libro -> string
-        * paginas -> numero de paginas del libro -> u16
-     */ 
-    pub fn agregar_libro(context: Context<NuevoLibro>, nombre: String, paginas: u16) -> Result<()> {
-        require!( // Medida de seguridad para identificar que SOLO el owner de la biblioteca sea el que hace cambios en ella
-            context.accounts.biblioteca.owner == context.accounts.owner.key(), // Condicion, true -> continua, false -> error
-            Errores::NoEresElOwner // Codigo de error, ver enum Errores
-        ); 
-
-        let libro = Libro { // Creacion de un struct tipo Libro
-            nombre,
-            paginas,
-            disponible: true,
+        let nueva_planta = Planta {
+            especie,
+            volumen_ml,
+            frecuencia_dias,
+            luz_directa,
         };
 
-        context.accounts.biblioteca.libros.push(libro); // Agrega el Libro al vector de libros de Biblioteca
-
-        Ok(()) // Transaccion exitosa
+        invernadero.plantas.push(nueva_planta);
+        msg!("Planta registrada.");
+        Ok(())
     }
 
-    //////////////////////////// Instruccion: Eliminar Libro /////////////////////////////////////
-    /*
-    Elimina un libro apartir de su nombre. Error si libro no existe, Error si vector vacio. 
+    pub fn editar_planta(
+        ctx: Context<GestionarInvernadero>, 
+        especie: String, 
+        nuevo_volumen: u16, 
+        nueva_frecuencia: u8,
+        nueva_luz_directa: bool
+    ) -> Result<()> {
+        let invernadero = &mut ctx.accounts.invernadero;
+        require!(invernadero.owner == ctx.accounts.owner.key(), Errores::NoEresElOwner);
 
-    Parametros de entrada:
-        * nombre -> Nombre del libro -> string
-     */
-    pub fn eliminar_libro(context: Context<NuevoLibro>, nombre: String) -> Result<()> {
-        require!( // Medida de seguridad
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
-
-        let libros = &mut context.accounts.biblioteca.libros; // Referencia mutable al vector de libros
-
-        for i in 0..libros.len() { // Se itera mediante el indice todo el contenido del vector en busca del libro a eliminar
-            if libros[i].nombre == nombre { // Si lo encuentra prodece a borrarlo mediante el metodo remove
-                libros.remove(i);
-                msg!("Libro {} eliminado!", nombre); // Mensaje de borrado exitoso
-                return Ok(()); // Transaccion exitosa
+        let lista = &mut invernadero.plantas;
+        for i in 0..lista.len() {
+            if lista[i].especie == especie {
+                lista[i].volumen_ml = nuevo_volumen;
+                lista[i].frecuencia_dias = nueva_frecuencia;
+                lista[i].luz_directa = nueva_luz_directa;
+                msg!("Cuidados de la planta actualizados.");
+                return Ok(());
             }
         }
-        Err(Errores::LibroNoExiste.into()) // Transaccion fallida, nunca encontro el libro
+        Err(Errores::PlantaNoExiste.into())
     }
 
-    //////////////////////////// Instruccion: Ver Libros /////////////////////////////////////
-    /*
-    Muestra en el log de la transaccion el contenido completo del vector de libros de la Biblioteca
+    pub fn eliminar_planta(ctx: Context<GestionarInvernadero>, especie: String) -> Result<()> {
+        let invernadero = &mut ctx.accounts.invernadero;
+        require!(invernadero.owner == ctx.accounts.owner.key(), Errores::NoEresElOwner);
 
-    Parametros de entrada:
-        Ninguno
-     */
-    pub fn ver_libros(context: Context<NuevoLibro>) -> Result<()> {
-        require!( // Medida de seguridad 
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
-
-        // :#? requiere que NuevoLibro tenga atributo Debug. Permite la visualizacion completa del vector en el log
-        msg!("La lista de libros actualmente es: {:#?}", context.accounts.biblioteca.libros); // Print en log
-        Ok(()) // Transaccion exitosa 
-    }
-
-    
-    //////////////////////////// Instruccion: Alternar Estado /////////////////////////////////////
-    /* 
-    Cambia el estado de disponible de false a true o de true a false.
-
-    Parametros de entrada:
-        * nombre -> Nombre del libro -> string
-     */
-    pub fn alternar_estado(context: Context<NuevoLibro>, nombre: String) -> Result<()> {
-        require!( // Medida de seguridad
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
-
-        let libros = &mut context.accounts.biblioteca.libros; // Referencia mutable al vector de libros
-        for i in 0..libros.len() { // Se itera mediante el indice el vector de libros
-            let estado = libros[i].disponible;  // Se almacena el estado del vector actual
-
-            if libros[i].nombre == nombre { // Si ecuentra el nombre del libro procede a cambiar el valor del estado 
-                let nuevo_estado = !estado;
-                libros[i].disponible = nuevo_estado;
-                msg!("El libro: {} ahora tiene un valor de disponibilidad: {}", nombre, nuevo_estado); // log print de la nueva disponibilidad
-                return Ok(()); // Transaccion exitosa
-            }
+        let lista = &mut invernadero.plantas;
+        let index = lista.iter().position(|x| x.especie == especie);
+        
+        if let Some(i) = index {
+            lista.remove(i);
+            msg!("Planta eliminada del invernadero.");
+            Ok(())
+        } else {
+            Err(Errores::PlantaNoExiste.into())
         }
-
-        Err(Errores::LibroNoExiste.into()) // Transaccion fallida, libro no existe
     }
 
+    pub fn leer_invernadero(ctx: Context<GestionarInvernadero>) -> Result<()> {
+        msg!("Invernadero: {}", ctx.accounts.invernadero.nombre_invernadero);
+        msg!("Datos: {:#?}", ctx.accounts.invernadero.plantas);
+        Ok(())
+    }
 }
 
-/*
-Codigos de error
-Todos los codigos se almacenan en un enum con la siguiente estructura:
-#[msg("MENSAJE DE ERROR")] (dentro de las comillas)
-NombreDelError, (En camel case)
-*/
-#[error_code]
-pub enum Errores {
-    #[msg("Error, no eres el propietario de la biblioteca que deseas modificar")]
-    NoEresElOwner,
-    #[msg("Error, el libro con el que deseas interactuar no existe")]
-    LibroNoExiste,
-}
-
-#[account] // Especifica que el strcut es una cuenta que se almacenara en la blockchain
-#[derive(InitSpace)] // Genera la constante INIT_SPACE y determina el espacio de almacenamiento necesario 
-pub struct Biblioteca { // Define la Biblioteca
-    owner: Pubkey, // Pubkey es un formato de llave publica de 32 bytes 
-
-    #[max_len(60)] // Cantidad maxima de caracteres del string: nombre
-    nombre: String,
-
-    #[max_len(10)] // Tamaño maximo del vector libros 
-    libros: Vec<Libro>,
-}
-
-/*
-Struct interno o secundario (No es una cuenta). Se define por derive y cuenta con los siguientes atributos:
-    * AnchorSerialize -> Permite guardar el struct en la cuenta 
-    * AnchorDeserialize -> Permite leer su contenido desde la cuenta 
-    * Clone -> Para copiar su contenido o valores 
-    * InitSpace -> Calcula el tamaño necesario para ser almacenado en la blockchain
-    * PartialEq -> Para usar sus valores y compararlos con "=="
-    * Debug -> Para mostrarlo en log con ":?" o ":#?"
-*/
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace, PartialEq, Debug)]
-pub struct Libro {
-    #[max_len(60)]
-    nombre: String,
-
-    // Los siguientes datos no rquieren de max_len porque ya estan definidos (numero de 16 bits y false o true)
-    paginas: u16, 
-
-    disponible: bool,
+pub struct Planta {
+    #[max_len(30)]
+    pub especie: String,
+    pub volumen_ml: u16,
+    pub frecuencia_dias: u8,
+    pub luz_directa: bool,
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct Invernadero {
+    pub owner: Pubkey,
+    #[max_len(40)]
+    pub nombre_invernadero: String,
+    #[max_len(15)] 
+    pub plantas: Vec<Planta>,
+}
 
-// Creacion de los contextos para las instrucciones (funciones)
-#[derive(Accounts)] // Especifica que este struct describe las cuentas que se requieren para determinada instruccion
-pub struct NuevaBiblioteca<'info> { // contexto de la instruccion
-    #[account(mut)] 
-    pub owner: Signer<'info>, // Se define que el owner como el que pagara la transaccion, por eso es mut, para que cambie el balance de la cuenta
+#[derive(Accounts)]
+pub struct CrearInvernadero<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
     #[account(
-        init, // Inidica que al llamar la instruccuion se creara una cuenta
-        // puede ser remplazado por "init_if_needed" para que solo se cree una vez por caller
-        payer = owner, // Se especifica que quien paga el llamado a la instruccion, en este caso llama la instruccion 
-        space = Biblioteca::INIT_SPACE + 8, // Se calcula el espacio requerido para almacenar el Solana Program On-Chain
-        seeds = [b"biblioteca", owner.key().as_ref()], // Se especifica que la cuenta es una PDA que depende de un string y el id del owner
-        bump // Metodo para determinar el el id de la biblioteca en base a lo anterior 
+        init,
+        payer = owner,
+        space = 8 + Invernadero::INIT_SPACE,
+        seeds = [b"invernadero", owner.key().as_ref()],
+        bump
     )]
-    pub biblioteca: Account<'info, Biblioteca>, // Se especifica que la cuenta creada (PDA) almacenara la biblioteca 
+    pub invernadero: Account<'info, Invernadero>,
 
-    pub system_program: Program<'info, System>, // Programa necesario para crear la cuenta 
+    pub system_program: Program<'info, System>,
 }
 
-// Contexto para la creacion y modificacion de libros 
-#[derive(Accounts)] // Especifica que este struct se requiere para todas las instrucciones relacionadas con la creacion o modificacion de Libro
-pub struct NuevoLibro<'info> {
-    pub owner: Signer<'info>, // El owner de la cuenta es quien paga la transaccion
-
+#[derive(Accounts)]
+pub struct GestionarInvernadero<'info> {
+    pub owner: Signer<'info>,
     #[account(mut)] 
-    pub biblioteca: Account<'info, Biblioteca>, // Se marca biblioteca como mutable porque se modificara tanto el vector como los libros que contiene
+    pub invernadero: Account<'info, Invernadero>,
+}
+
+#[error_code]
+pub enum Errores {
+    #[msg("No tienes permisos.")]
+    NoEresElOwner,
+    #[msg("La planta no existe en el invernadero.")]
+    PlantaNoExiste,
 }
